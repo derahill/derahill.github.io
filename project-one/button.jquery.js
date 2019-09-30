c9526738a11fe3bf7ba5e9a570a98d217b3d1ed5
derahill <54783991+derahill@users.noreply.github.com>
2019-09-30 10:09:21 -0400

    final project letter
    final commit

--- project-one/button.jquery.js
+++ project-one/button.jquery.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
-  $("#butta").click(function(){
+  $("#butta").click(function() {
     $("#letteranalysis").toggleClass("invisible");
-
-
+    $("#upperdlighter").addClass("upperdstandard");
+    $("#butta").css('background-color', '#CC7E3D');
+    $("#butt1").css('background-color', '#EFD6AE');
   });
-
+  $("#butt1").click(function() {
+    $("#letteranalysis1").toggleClass("invisible");
+    $("#upperdlighter").addClass("upperdlight");
+    $("#upperdlighter").removeClass("upperdstandard");
+    $("#butt1").css('background-color', '#CC7E3D');
+    $("#butta").css('background-color', '#EFD6AE');
+  });
+  $("#buttb").click(function() {
+    $("#letteranalysis2").toggleClass("invisible");
+    $("#lowerdlighter").addClass("upperdstandard");
+    $("#buttb").css('background-color', '#CC7E3D');
+    $("#butt2").css('background-color', '#EFD6AE');
+  });
+  $("#butt2").click(function() {
+    $("#letteranalysis3").toggleClass("invisible");
+    $("#lowerdlighter").addClass("upperdlight");
+    $("#lowerdlighter").removeClass("upperdstandard");
+    $("#butt2").css('background-color', '#CC7E3D');
+    $("#buttb").css('background-color', '#EFD6AE');
+  });
 });
