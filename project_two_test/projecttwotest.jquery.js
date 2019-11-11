b70a92a2dfebdfa971eb9fbc3c10eb5b6e236e8f
derahill <54783991+derahill@users.noreply.github.com>
2019-11-11 12:47:21 -0500

    github link fix
    fix

--- project_two_test/projecttwotest.jquery.js
+++ project_two_test/projecttwotest.jquery.js
@@ -2,11 +2,14 @@
 // $('.parallax-window3').parallax({imageSrc: 'project_two_test/assets/dayskybackground1.jpg'})
 //
 // // $('.parallax-window2').parallax({imageSrc: 'project_two_test/darksky1.png'});
-
-
-
-
-$('#box').click(function() {
- $("#btn").addclass("invisible");
-});
+// 
+// $('#').click(function() {
+//   window.location='http://www.example.com';
+// });
+//
+//
+//
+// $('#box').click(function() {
+//  $("#btn").addclass("invisible");
+// });
 // https://codereview.stackexchange.com/questions/134106/changing-color-on-button-click
