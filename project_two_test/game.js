bec2b809323f9cb01aded58b9b62d38f0197857c
derahill <54783991+derahill@users.noreply.github.com>
2019-11-11 10:41:49 -0500

    github js fix
    js fix for project 2

--- project_two_test/game.js
+++ project_two_test/game.js
@@ -1,7 +1,7 @@
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 const optionsdictionary = {
-  "Start Game": {value: 0, color:"#DF9DB5", backgroundColor:"#DF9DB5"},
+  "Start Game": {value: 0, color:"#DF9DB5", backgroundColor:"#DF9DB5", href:"index2.html"},
   "Author": {value: 0, color:"#DF9DB5", backgroundColor:"#DF9DB5"},
   "Water": {value: 1, color:"#2C5C32", backgroundColor:"#DF9DB5",  opacity: '.9'},
   "Knife": {value: 3, color:"#BC3C3F"},
@@ -47,17 +47,7 @@ const optionsdictionary = {
   "HELP": {value:1, color:"#E6CF7E"},
   "Marking Trees": {value:1, color:"#E6CF7E"},
 }
-// var button1 = document.getElementById("green");
-// var button2 = document.getElementById("red");
-// var button3 = document.getElementById("gold");
-// var button4 = document.getElementById("green");
-// counter for good survival choices
 
-// counter for aggressive survival choices
-// var str_counter_1 = count++;
-// // counter for bad survival choices
-// var str_counter_2 = count++;
-// var hope = document.getElementById("btn");
 var counter = 0;
 
 function add() {
@@ -73,17 +63,7 @@ function add5() {
   counter += 5;
   return counter;
 }
-//
-// if (showTextNode(34)) {
-//
-//     // if (counter <= 20) {
-//     //     showTextNode(37)
-//     // }
-//     //
-// }
-
-
-
+// WIP counter
   // }
   // if (showTextNode(34) && (counter >= 40 )) {
   //   showTextNode(37)
@@ -113,17 +93,13 @@ function showTextNode(textNodeIndex) {
 
   if (textNode.timeofday == "day") {
     console.log(textNode.timeofday)
-    // console.log(textNode.id)
-    // document.getElementById('back').setAttribute("data-image-src", "assets/dayskybackground1.jpg")
     document.getElementById('back1').style.display = "block"
     document.getElementById('back').style.display = "none"
-    // console.log("printme")
   }
   else if (textNode.timeofday == "night") {
     console.log(textNode.timeofday)
     document.getElementById('back').style.display = "block"
       document.getElementById('back1').style.display = "none"
-    // document.getElementById("back").setAttribute("data-image-src", "assets/darksky1.png")
   }
 
   textNode.options.forEach(option => {
@@ -137,80 +113,15 @@ function showTextNode(textNodeIndex) {
       button.addEventListener('click', () => selectOption(option, option.text))
       optionButtonsElement.appendChild(button)
       button.style.backgroundColor = optionsdictionary[option.text].color
-      // var shit = optionsdictionary[option.text].color
-      // var x = document.getElementsByClassName("btn:hover")
-      // Array.from(x).forEach(function(mybutton, shit) {
-      //   // mybutton.style.backgroundColor = shit
-      //   // mybutton.style.color = shit
-      //   console.log("print")
-      // })
-      //
-      // document.getElementsByClassName("contain").style.color.forEach(button => {
-      //   contain.style.color = optionsdictionary[option.text].color;
-      // }
+
         }
-        // optionsdictionary[option.text].color);
-      // forEach(button) in optionButtonsElements {
-      //   document.getElementsByClassName("contain").style.color = optionsdictionary[option.text].color;
-      // }
+
       document.getElementById("box").style.opacity = optionsdictionary[option.text].opacity
-      // document.getElementById("box").style["boxShadow"] = optionsdictionary[option.text].color
       document.getElementById("box").style.background = optionsdictionary[option.text].backgroundColor
-      // document.getElementById("hope").style.background = optionsdictionary[option.text].color
-      // document.getElementById("box").style.background = optionsdictionary[option.text].color
-      // var shit = optionsdictionary[option.text].color
-      // var x = document.getElementsByClassName("btn:hover")
-      // console.log(x)
-      //
-      // Array.from(x).forEach(function(mybutton, shit) {
-      //   // mybutton.style.backgroundColor = shit
-      //   // mybutton.style.color = shit
-      //   console.log("print")
-      // })
-      // document.getElementById("box").style.background = optionsdictionary[option.text].color
 
-      // document.getElementById("hope").style["box-shadow"] = optionsdictionary[option.text].color
 
-      // if (option.text == "Water") {
-      //   button.style.backgroundColor = 'blue';
-      //   button.imageSrc =
-      // }
-
-    // for (button) in optionButtonsElement {
-    //     document.getElementById("btn").style["box-shadow"].color = optionsdictionary[option.text].color
-
-    // }
   })
-  //
-  // forEach(button) {
-  //   document.getElementById("btn").style["box-shadow"].color = optionsdictionary[option.text].color
-  //
-  // }
-  //
-  // var comhope = window.getComputedStyle(element, null);
-  // hope.addEventListener("click",function(){
-  //     if(showTextNode(3)) {
-  //         // toggle = true;
-  //         hope.style.border = "10px solid #FF0000";
-  //         hope.style.backgroundColor = "#AA3030";
-  //     } else {
-  //         // toggle = false;
-  //         hope.style.border = "10px solid #9676E8";
-  //         hope.style.backgroundColor = "#B299F2";
-  //     }
-  // })
-  // var col=document.getElementById("btn");
-// // left off with this, idk why its not working, but its a variable that gets the css class of the button w the tag, but cant change color.
-//   // var hope = document.getElementsByClassName('btn');
-    // col.style.backgroundColor = 'blue'; //this for some reason has to be one for the buttons to appear
-//   // button.addEventListener('click', () => selectOption(option))
-  // if (showTextNode(2)){
-  //   add();
-    // const col.classList.add('button1');
-//
-//     // document.button.style.background = blue;
-//     // col.Element.style.backgroundColor="#f47121";
-  // }
+
 }
 
 
@@ -243,13 +154,10 @@ var textNodes = [
   text: "Lorem Ipsum \n \n \n A text-based survival game to teach basic survival skills",
   options: [
     {
-      text: 'Start Game',
+      text: 'Restart',
       nextText: 1
-    },
-    {
-      text: 'Author',
-      nextText: 37
     }
+
   ]
 },
   {
@@ -357,7 +265,6 @@ var textNodes = [
       },
       {
         text: 'Fight',
-        // requiredState: (currentState) => currentState.Knife || currentState.Light || currentState.Food || currentState.Water,
         nextText: 11
       },
     ]
